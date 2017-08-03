515f93301e8f501e77cff4d2fe7df04af53a3cd4
arokhin <artem.rokhin@gmail.com>
2017-08-03 18:05:11 +0300

    Create reloader.sh

---
+++ reloader.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ONE_TIME=false
+if [ ! -z "$1" ] && [ "$1" = "oneTime" ] ; then
+    ONE_TIME=true
+fi
+
+while true ; do
+  python2.7 /loadbalancer.py http://${MONITORING_HOST}:${MONITORING_PORT}/monitoring/frontends /conf/haproxy/haproxy.cfg.tmpl /usr/local/etc/haproxy/haproxy.cfg /var/run/haproxy-systemd-wrapper.pid
+  if [ "$ONE_TIME" = true ]; then
+     break;
+  fi
+  sleep 2
+done
