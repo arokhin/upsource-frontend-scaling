01ec4920170529f51791c3a712956052bb0ac770
arokhin <artem.rokhin@gmail.com>
2017-08-04 16:43:38 +0300

    Update reloader.sh

--- reloader.sh
+++ reloader.sh
@@ -6,7 +6,7 @@ if [ ! -z "$1" ] && [ "$1" = "oneTime" ] ; then
 fi
 
 while true ; do
-  python2.7 /loadbalancer.py http://${MONITORING_HOST}:${MONITORING_PORT}/monitoring/frontends /conf/haproxy/haproxy.cfg.tmpl /usr/local/etc/haproxy/haproxy.cfg /var/run/haproxy-systemd-wrapper.pid
+  python2.7 /loadbalancer.py http://${MONITORING_HOST}:${UPSOURCE_MONITORING_LISTEN_PORT}/monitoring/frontends /conf/haproxy/haproxy.cfg.tmpl /usr/local/etc/haproxy/haproxy.cfg /var/run/haproxy-systemd-wrapper.pid
   if [ "$ONE_TIME" = true ]; then
      break;
   fi
