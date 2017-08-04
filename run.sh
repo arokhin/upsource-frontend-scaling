d9426c73808f2275844c460a4d36832e4408f346
arokhin <artem.rokhin@gmail.com>
2017-08-04 17:09:15 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,16 +4,18 @@ set -e
 
 MONITORING_HOST=${MONITORING_HOST:-"monitoring"}
 MONITORING_PORT=${UPSOURCE_MONITORING_LISTEN_PORT:-"10080"}
+HAPROXY_CONF_LOCATION=${HAPROXY_CONF_LOCATION:-"/usr/local/etc/haproxy"}
+HAPROXY_SCRIPTS_LOCATION=${HAPROXY_SCRIPTS_LOCATION:-"/opt/upsource-haproxy"}
 
 # Init haproxy config
-python -c 'import loadbalancer; loadbalancer.init_config()' http://${MONITORING_HOST}:${MONITORING_PORT}/monitoring/frontends /conf/haproxy/haproxy.cfg.tmpl /usr/local/etc/haproxy/haproxy.cfg
+python -c 'import loadbalancer; loadbalancer.init_config()' http://${MONITORING_HOST}:${MONITORING_PORT}/monitoring/frontends ${HAPROXY_SCRIPTS_LOCATION}/conf/haproxy/haproxy.cfg.tmpl ${HAPROXY_CONF_LOCATION}/haproxy.cfg
 
 # haproxy-systemd-wrapper is a built-in haproxy wrapper
-"$(which haproxy-systemd-wrapper)" -p /var/run/haproxy.pid -f /usr/local/etc/haproxy/haproxy.cfg "$@" & echo $! > /var/run/haproxy-systemd-wrapper.pid
+"$(which haproxy-systemd-wrapper)" -p /var/run/haproxy.pid -f ${HAPROXY_CONF_LOCATION}/haproxy.cfg "$@" & echo $! > /var/run/haproxy-systemd-wrapper.pid
 #haproxy -c -f /usr/local/etc/haproxy/haproxy.cfg
 
 #/reloader.sh oneTime
 # schedule reloading of configs
-/reloader.sh &
+${HAPROXY_SCRIPTS_LOCATION}/reloader.sh &
 
 wait
