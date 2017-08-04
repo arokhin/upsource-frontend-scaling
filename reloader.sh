85a58cb1a9e2bcc04cf7d7b1335edb412e790891
arokhin <artem.rokhin@gmail.com>
2017-08-04 17:01:02 +0300

    Update reloader.sh

--- reloader.sh
+++ reloader.sh
@@ -5,6 +5,7 @@ set -e
 MONITORING_HOST=${MONITORING_HOST:-"monitoring"}
 MONITORING_PORT=${UPSOURCE_MONITORING_LISTEN_PORT:-"10080"}
 HAPROXY_CONF_LOCATION=${HAPROXY_CONF_LOCATION:-"/usr/local/etc/haproxy"}
+HAPROXY_SCRIPTS_LOCATION=${HAPROXY_SCRIPTS_LOCATION:-"/opt/upsource-haproxy"}
 
 ONE_TIME=false
 if [ ! -z "$1" ] && [ "$1" = "oneTime" ] ; then
@@ -12,7 +13,7 @@ if [ ! -z "$1" ] && [ "$1" = "oneTime" ] ; then
 fi
 
 while true ; do
-  python2.7 /loadbalancer.py http://${MONITORING_HOST}:${MONITORING_PORT}/monitoring/frontends /conf/haproxy/haproxy.cfg.tmpl ${HAPROXY_CONF_LOCATION}/haproxy.cfg /var/run/haproxy-systemd-wrapper.pid
+  python2.7 ${HAPROXY_SCRIPTS_LOCATION}/loadbalancer.py http://${MONITORING_HOST}:${MONITORING_PORT}/monitoring/frontends /conf/haproxy/haproxy.cfg.tmpl ${HAPROXY_CONF_LOCATION}/haproxy.cfg /var/run/haproxy-systemd-wrapper.pid
   if [ "$ONE_TIME" = true ]; then
      break;
   fi
